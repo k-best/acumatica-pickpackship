83f55ce170811804400d2331683e017b0c5f6720
Hughes Beausejour <hbeausejour@acumatica.ca>
2017-02-02 10:57:27 -0500

    Adding UI for new SOSetup configuration

---
+++ PX.Objects.WM/SOSetupMaintExt.cs
@@ -0,0 +1,24 @@
+﻿using PX.Data;
+using PX.Objects.CS;
+using PX.Objects.SO;
+
+namespace PX.Objects.WM
+{
+    public class SOSetupMaintExt : PXGraphExtension<SOSetupMaint>
+    {
+        protected virtual void SOSetup_RowSelected(PXCache sender, PXRowSelectedEventArgs e)
+        {
+            SOSetup setup = (SOSetup)e.Row;
+
+            if (setup != null && !PXAccess.FeatureInstalled<FeaturesSet.warehouseLocation>())
+            {
+                SOSetupExt setupExt = PXCache<SOSetup>.GetExtension<SOSetupExt>(setup);
+
+                if (setupExt != null)
+                {
+                    PXUIFieldAttribute.SetVisible<SOSetupExt.usePickLocation>(sender, setup, false);
+                }
+            }
+        }
+    }
+}
